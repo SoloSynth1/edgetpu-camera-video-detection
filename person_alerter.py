4b4e814c9ad4dd2d275b16921207ac05e567b7b2
SoloSynth1 <orixay@hotmail.com>
2020-09-28 00:36:44 +0800

    add periodic alerting logic

--- person_alerter.py
+++ person_alerter.py
@@ -1,37 +1,9 @@
-# Copyright 2019 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""A demo that runs object detection on camera frames using OpenCV.
-
-TEST_DATA=../all_models
-
-Run face detection model:
-python3 detect.py \
-  --model ${TEST_DATA}/mobilenet_ssd_v2_face_quant_postprocess_edgetpu.tflite
-
-Run coco model:
-python3 detect.py \
-  --model ${TEST_DATA}/mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite \
-  --labels ${TEST_DATA}/coco_labels.txt
-
-"""
 import os
-import re
 import argparse
 import collections
 import json
 import datetime
+import time
 
 from dotenv import load_dotenv
 import urllib3
@@ -50,18 +22,13 @@ PERSON_COCO_INDEX = 0
 
 Object = collections.namedtuple('Object', ['id', 'score', 'bbox'])
 
-def load_labels(path):
-    p = re.compile(r'\s*(\d+)(.+)')
-    with open(path, 'r', encoding='utf-8') as f:
-       lines = (p.match(line).groups() for line in f.readlines())
-       return {int(num): text.strip() for num, text in lines}
-
 class BBox(collections.namedtuple('BBox', ['xmin', 'ymin', 'xmax', 'ymax'])):
     """Bounding box.
     Represents a rectangle which sides are either vertical or horizontal, parallel
     to the x or y axis.
     """
     __slots__ = ()
+
 
 def get_output(interpreter, score_threshold, top_k, image_scale=1.0):
     """Returns list of detected objects."""
@@ -82,31 +49,38 @@ def get_output(interpreter, score_threshold, top_k, image_scale=1.0):
 
     return [make(i) for i in range(top_k) if scores[i] >= score_threshold]
 
+
 def send_alert(alert_str):
     data = {'text': alert_str}
     http.request('POST', WEBHOOK_URL, body=json.dumps(data).encode('utf-8'), headers=WEBHOOK_HEADERS)
 
+
 def main():
     default_model_dir = '../all_models'
     default_model = 'mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite'
-    default_labels = 'coco_labels.txt'
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', help='.tflite model path',
-                        default=os.path.join(default_model_dir,default_model))
-    parser.add_argument('--labels', help='label file path',
-                        default=os.path.join(default_model_dir, default_labels))
+                        default=os.path.join(default_model_dir, default_model))
     parser.add_argument('--top_k', type=int, default=10,
                         help='number of categories with highest score to display')
     parser.add_argument('--camera_idx', type=int, help='Index of which video source to use. ', default = 0)
     parser.add_argument('--threshold', type=float, default=0.5,
                         help='classifier score threshold')
+    parser.add_argument('--')
     args = parser.parse_args()
 
-    print('Loading {} with {} labels.'.format(args.model, args.labels))
+    print('Loading {}.'.format(args.model))
     interpreter = common.make_interpreter(args.model)
     interpreter.allocate_tensors()
 
     cap = cv2.VideoCapture(args.camera_idx)
+
+
+    first_hit = 0
+    stop_until = 0
+    double_confirm_min = 3
+    double_confirm_max = 10
+    refractory_period = 60
 
     while cap.isOpened():
         ret, frame = cap.read()
@@ -122,8 +96,14 @@ def main():
         objs = get_output(interpreter, score_threshold=args.threshold, top_k=args.top_k)
         persons = list(filter(lambda x: x.id == PERSON_COCO_INDEX, objs))
         if persons:
-            send_alert("person detected at {}".format(datetime.datetime.now().isoformat()))
-            break
+            current_time = time.time()
+            if current_time > stop_until:
+                if (current_time - first_hit) > double_confirm_max:
+                    first_hit = current_time
+                elif (current_time - first_hit < double_confirm_max) and (current_time - first_hit >= double_confirm_min):
+                    send_alert("person detected at {}".format(datetime.datetime.now().isoformat()))
+                    print("alerted.")
+                    stop_until = current_time + refractory_period
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
